d2dc2c8acd7297b28b5cc9a5d59524b22ab1803b
R01085B-Limaylla <r01085b@ms.upla.edu.pe>
2025-11-26 13:32:52 -0500

    aver

--- script.js
+++ script.js
@@ -26,7 +26,6 @@ window.ADMINS = (window.ADMINS && window.ADMINS.length)
       "rosario@lamarquesina.pe",
       "sebastian@lamarquesina.pe",
       "sujey@lamarquesina.pe",
-      "paulmarcos@lamarquesina.pe",
     ];
 
 // === Helpers de sesión / auth ===
